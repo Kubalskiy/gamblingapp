6f477a3e05eaa4d2c167c974dc50bebb0287fc81
Ihor.Kubalaskyi <info@kubemake.com>
2016-07-19 12:49:25 +0300

    round_button

--- views/game/index.php
+++ views/game/index.php
@@ -2,62 +2,85 @@
 
 /* @var $this yii\web\View */
 
-$this->title = \Yii::t('app','Random game');
+$this->title = \Yii::t('app', 'Random game');
 $hasGame = $data['hasGame'];
 ?>
-<script>var isGuest = <?php echo \Yii::$app->user->isGuest ? '1' : '0';?></script>
+<script>var isGuest = <?php echo \Yii::$app->user->isGuest ? '1' : '0'; ?></script>
 <div class="jumbotron">
-    <h1><?=\Yii::t('app', 'Hello friend!')?></h1>
+    <h1><?= \Yii::t('app', 'Hello friend!') ?></h1>
+
     <div class="row">
-        <div id="messages" class="col-md-7"><?=$msg?></div>
+        <div id="messages" class="col-md-7"><?= $msg ?></div>
         <div class="col-md-5">
             <div>
-                <span style="width:95px;display:inline-block;"><?=\Yii::t('app','Your Drop')?></span>
+                <span style="width:95px;display:inline-block;"><?= \Yii::t('app', 'Your Drop') ?></span>
                 <span id="yourDrop"></span>
             </div>
             <div>
-                <span style="width:95px;display:inline-block;"><?=\Yii::t('app','Opponent Drop')?></span>
+                <span style="width:95px;display:inline-block;"><?= \Yii::t('app', 'Opponent Drop') ?></span>
                 <span id="oppDrop"></span>
             </div>
         </div>
     </div>
     <div class="lead">
         <div class="row mb">
-            <div class="col-sm-3 col-xs-6"><?=\Yii::t('app', 'My credits')?></div>
+            <div class="col-sm-3 col-xs-6"><?= \Yii::t('app', 'My credits') ?></div>
             <div class="col-sm-4">
-                <span id="credits"><?=@$data['credits']?></span>
-                <span class="pull-right">(<a class="has-spinner" id="creditRequest" href="javascript:creditRequest();">попросить кредитов<span class="spinner"><i class="fa fa-cog fa-spin"></i></span></a>)</span>
+                <span id="credits"><?= @$data['credits'] ?></span>
+                <span class="pull-right">(<a class="has-spinner" id="creditRequest" href="javascript:creditRequest();">попросить
+                        кредитов<span class="spinner"><i class="fa fa-cog fa-spin"></i></span></a>)</span>
             </div>
         </div>
         <div class="row"></div>
         <div class="row">
-            <div class="col-sm-3 col-xs-6"><?=\Yii::t('app', 'Bet:')?></div>
+            <div class="col-sm-3 col-xs-6"><?= \Yii::t('app', 'Bet:') ?></div>
             <div class="col-sm-3 col-md-2">
                 <div class="betbtn input-group">
                     <span id="more" class="input-group-addon">+</span>
-                    <input id="bet" type="text" class="form-control" value="<?php echo (\Yii::$app->session->get('fixedBet')>0) ? \Yii::$app->session->get('fixedBet') : 1; ?>" onchange="if (this.value>10) this.value=10;if ($('#fixedbet').prop('checked')) $('#fixedbet').click();">
+                    <input id="bet" type="text" class="form-control"
+                           value="<?php echo (\Yii::$app->session->get('fixedBet') > 0) ? \Yii::$app->session->get('fixedBet') : 1; ?>"
+                           onchange="if (this.value>10) this.value=10;if ($('#fixedbet').prop('checked')) $('#fixedbet').click();">
                     <span id="less" class="input-group-addon">-</span>
                 </div>
             </div>
             <div class="col-sm-3">
-                <label for="fixedbet"><input type="checkbox" name="fixedbet" id="fixedbet"<?php echo \Yii::$app->session->get('fixedBet') ? ' checked' : ''; ?>> запомнить</label>
+                <label for="fixedbet"><input type="checkbox" name="fixedbet"
+                                             id="fixedbet"<?php echo \Yii::$app->session->get('fixedBet') ? ' checked' : ''; ?>>
+                    запомнить</label>
             </div>
         </div>
     </div>
 
     <p>
         <?php if (!$data['credits']): ?>
-            <?php if (\Yii::$app->user->isGuest): ?>
-                <button class="btn btn-lg btn-success input-lg" name="play" id="play" data-state="needRegister"><?=\Yii::t('app', 'Please register to continue')?></button>
-            <?php else: ?>
-                <button class="btn btn-lg btn-success" name="play" id="play" data-state="ready" disabled="disabled"><?=\Yii::t('app', 'We need more Gold! (C)')?></button>
-            <?php endif; ?>
-        <?php elseif (!$hasGame): ?>
-            <button class="btn btn-lg btn-success input-lg" name="play" id="play" data-state="ready"><?=\Yii::t('app', 'Play game')?></button>
-        <?php else: ?>
-            <button class="btn btn-lg btn-success" name="play" id="play" data-state="waitOpponent" disabled="disabled"><?=\Yii::t('app', 'Wait for opponent')?></button>
-        <?php endif; ?>
-        <button class="btn btn-lg btn-default" name="playmoney" id="playmoney" data-toggle="modal" data-target="#mailModal"><?=\Yii::t('app','I am lucky, Bet money')?></button>
+        <?php if (\Yii::$app->user->isGuest): ?>
+
+    <div class="round-button">
+        <div class="round-button-circle">
+            <button class="round-button" name="play" id="play"
+                    data-state="needRegister"><?= \Yii::t('app', 'Please register to continue') ?></button>
+        </div>
+    </div>
+    <?php else: ?>
+        <button class="btn btn-lg btn-success" name="play" id="play" data-state="ready"
+                disabled="disabled"><?= \Yii::t('app', 'We need more Gold! (C)') ?></button>
+    <?php
+    endif; ?>
+    <?php elseif (!$hasGame): ?>
+        <div class="round-button">
+            <div class="round-button-circle">
+                <button class="round-button" name="play" id="play"
+                        data-state="needRegister"><?= \Yii::t('app', 'Play game') ?></button>
+            </div>
+        </div>
+    <?php
+    else: ?>
+        <button class="btn btn-lg btn-success" name="play" id="play" data-state="waitOpponent"
+                disabled="disabled"><?= \Yii::t('app', 'Wait for opponent') ?></button>
+    <?php
+    endif; ?>
+    <button class="btn btn-lg btn-default" name="playmoney" id="playmoney" data-toggle="modal"
+            data-target="#mailModal"><?= \Yii::t('app', 'I am lucky, Bet money') ?></button>
     </p>
 </div>
 
@@ -68,16 +91,19 @@ $hasGame = $data['hasGame'];
             <form method="post">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel"><?=\Yii::t('app', 'Get your email')?></h4>
+                    <h4 class="modal-title" id="myModalLabel"><?= \Yii::t('app', 'Get your email') ?></h4>
                 </div>
                 <div class="modal-body">
-                    <?=\Yii::t('app', 'Your invite will be send to email')?>
-                    <input class="form-input" name="email" type="email" id="email" required value="<?=@$data['email']?>"/>
+                    <?= \Yii::t('app', 'Your invite will be send to email') ?>
+                    <input class="form-input" name="email" type="email" id="email" required
+                           value="<?= @$data['email'] ?>"/>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal"><?=\Yii::t('app', 'Close')?></button>
-                    <input type="hidden" name="_csrf" value="<?=Yii::$app->request->getCsrfToken()?>" />
-                    <button type="submit" id="sendInvite" name="sendInvite" class="btn btn-primary"><?=\Yii::t('app', 'Get invite')?></button>
+                    <button type="button" class="btn btn-default"
+                            data-dismiss="modal"><?= \Yii::t('app', 'Close') ?></button>
+                    <input type="hidden" name="_csrf" value="<?= Yii::$app->request->getCsrfToken() ?>"/>
+                    <button type="submit" id="sendInvite" name="sendInvite"
+                            class="btn btn-primary"><?= \Yii::t('app', 'Get invite') ?></button>
                 </div>
             </form>
         </div>
